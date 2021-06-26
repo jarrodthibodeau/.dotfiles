091607155c2cb848824a685ee5f0a69807a58a3c
Jarrod Thibodeau <thibodeaujz@gmail.com>
2021-06-26 13:19:50 -0400

    actually using a formatter

--- lua/jzt/telescope.lua
+++ lua/jzt/telescope.lua
@@ -1,19 +1,18 @@
-require('telescope').setup {
-   defaults = {
-    file_sorter = require('telescope.sorters').get_fzy_sorter,
-    prompt_prefix = ' >',
-    color_devicons = true,
-
-    file_preview = require('telescope.previewers').vim_buffer_cat.new,
-    grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
-    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new
-   },
-   extensions = {
+require("telescope").setup {
+    defaults = {
+        file_sorter = require("telescope.sorters").get_fzy_sorter,
+        prompt_prefix = " >",
+        color_devicons = true,
+        file_preview = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new
+    },
+    extensions = {
         fzy_native = {
             override_generic_sorter = false,
-            override_file_sorter = true,
+            override_file_sorter = true
         }
-   }
+    }
 }
 
-require('telescope').load_extension('fzy_native');
+require("telescope").load_extension("fzy_native")
